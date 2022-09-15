8acd5ab1f01986c864b66ea280b4d4ea51a75f43
ckp-0217 <ckp0217@foxmail.com>
2022-09-15 17:20:09 +0800

    abi生成

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('hardhat-abi-exporter');
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.5.16" ,
+  abiExporter: {
+    path: './abi',
+    runOnCompile: true,
+    clear: true,
+    flat: false,
+    spacing: 2,
+    pretty: true,
+  },
+
 };
